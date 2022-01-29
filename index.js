1c64d49e369c2ac6056c27ca4b5e6424d137f296
Vishesh <vishesh0502.cse19@chitkara.edu.in>
2022-01-29 15:36:08 +0530

    setup static files access

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-
+const expressLayouts = require("express-ejs-layouts");
+app.use(express.static("./assets"));
+app.use(expressLayouts);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 app.use("/", require("./routes/index"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error in running the server: ${err}`);
