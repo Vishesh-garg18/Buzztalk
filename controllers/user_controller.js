aac4bd6b8c2159e5a4a55d763fee6e0408c63bea
Vishesh <vishesh0502.cse19@chitkara.edu.in>
2022-01-31 10:24:08 +0530

    authentication

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,46 @@
+const User = require("../models/user");
+
 module.exports.profile = function (req, res) {
-  return res.end("<h1>User profile</h1>");
+  return res.render("user_profile", {
+    title: "User Profile",
+  });
 };
+
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Codial | Sign Up",
+  });
+};
+
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Codial | Sign In",
+  });
+};
+
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user in signing up");
+      return;
+    }
+
+    if (!user) {
+      User.create(req.body, function (errr, user) {
+        if (err) {
+          console.log("error in creating user");
+          return;
+        }
+        return res.redirect("/user/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
+
+module.exports.createSession = function (req, res) {};
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const cookieParser = require("cookie-parser");
 const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
+
 app.use(express.static("./assets"));
+
+app.use(express.urlencoded());
+app.use(cookieParser());
+
 app.use(expressLayouts);
 app.set("layout extractStyles", true);
 app.set("layout extractScripts", true);
-app.use("/", require("./routes/index"));
+app.use("/", require("./routes"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.listen(port, function (err) {
