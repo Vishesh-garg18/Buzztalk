035eba1d77ce7933ab1cb408ae98b9059859057b
Vishesh <vishesh0502.cse19@chitkara.edu.in>
2022-02-03 14:38:34 +0530

    Passport authentication

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,18 +1,37 @@
 const User = require("../models/user");
 
 module.exports.profile = function (req, res) {
-  return res.render("user_profile", {
-    title: "User Profile",
-  });
+  if (req.cookies.user_id) {
+    User.findById(req.cookies.user_id, function (err, user) {
+      if (user) {
+        return res.render("user_profile", {
+          title: "User Profile",
+          user: user,
+        });
+      } else {
+        return res.redirect("/user/sign-in");
+      }
+    });
+  } else {
+    return res.redirect("/user/sign-in");
+  }
 };
 
 module.exports.signUp = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/user/profile");
+  }
+
   return res.render("user_sign_up", {
     title: "Codial | Sign Up",
   });
 };
 
 module.exports.signIn = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/user/profile");
+  }
+
   return res.render("user_sign_in", {
     title: "Codial | Sign In",
   });
@@ -43,4 +62,11 @@ module.exports.create = function (req, res) {
   });
 };
 
-module.exports.createSession = function (req, res) {};
+module.exports.createSession = function (req, res) {
+  return res.redirect("/");
+};
+
+module.exports.destroySession = function (req, res) {
+  req.logout();
+  return res.redirect("/");
+};
